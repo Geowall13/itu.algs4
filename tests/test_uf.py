00656f51810f745a837c8d10ab674db4b65400ea
Geowall13 <rasmushr13@hotmail.com>
2020-04-01 14:41:47 +0200

    More uf stuff

--- tests/test_uf.py
+++ tests/test_uf.py
@@ -1,14 +1,21 @@
 import pytest
 
+
 from itu.algs4.fundamentals.uf import UF, QuickUnionUF, WeightedQuickUnionUF, QuickFindUF
 
-
-def test_initial_count():
+def test_count():
     for i in range(100):
-        assert UF(i).count() == i
-        assert QuickUnionUF(i).count() == i
-        assert WeightedQuickUnionUF(i).count() == i
-        assert QuickFindUF(i).count() == i
+        uf = UF(i)
+        assert uf.count() == i
+
+        quuf = QuickUnionUF(i)
+        assert quuf.count() == i
+
+        wquuf = WeightedQuickUnionUF(i)
+        assert wquuf.count() == i
+
+        qfuf = QuickFindUF(i)
+        assert qfuf.count() == i
 
 @pytest.mark.parametrize(
     "uf",
@@ -20,6 +27,22 @@ def test_initial_count():
     ]
 )
 def test_initial_connected(uf):
-    for i in range(99):
-        for j in range(i+1,99-i):
+    for i in range(100):
+        for j in range(i+1, 100-i):
             assert not uf.connected(i,j)
+
+@pytest.mark.parametrize(
+    "uf",
+    [
+        UF(100),
+        QuickUnionUF(100),
+        QuickFindUF(100),
+        WeightedQuickUnionUF(100)
+    ]
+)
+def test_union(uf):
+    for i in range(1, 100):
+        uf.union(0, i)
+        assert uf.connected(i, i-1)
+        assert uf.count() == 100-i
+
